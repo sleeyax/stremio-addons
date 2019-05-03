019aedb1a2bf5bc986e2e9929729b32b3b9676bf
Sleeyax <yourd3veloper@gmail.com>
2019-05-03 02:28:25 +0200

    addon/dlive: recursively fetch all categories

--- packages/addons/stremio-dlive-addon/src/dlive.js
+++ packages/addons/stremio-dlive-addon/src/dlive.js
@@ -4,20 +4,28 @@ const constants = require("./constants");
 class Dlive {
     constructor() {}
 
-    getCategories() {
+    getCategories(offset = -1, count = 50, currentList = []) {
         return request(constants.url, {
             method: "POST",
             json: {
                 operationName: "BrowsePageSearchCategory",
                 variables: {
                     text: "",
-                    first: 9999999999,
-                    after: "0"
+                    first: count,
+                    after: offset + ""
                 },
                 query: "query BrowsePageSearchCategory($text: String!, $first: Int, $after: String) {\n  search(text: $text) {\n    trendingCategories(first: $first, after: $after) {\n      ...HomeCategoriesFrag\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment HomeCategoriesFrag on CategoryConnection {\n  pageInfo {\n    endCursor\n    hasNextPage\n    __typename\n  }\n  list {\n    ...VCategoryCardFrag\n    __typename\n  }\n  __typename\n}\n\nfragment VCategoryCardFrag on Category {\n  id\n  backendID\n  title\n  imgUrl\n  watchingCount\n  __typename\n}\n"
             }
         }).then((response) => {
-            return response["data"]["search"]["trendingCategories"]["list"];
+            currentList = currentList.concat(response["data"]["search"]["trendingCategories"]["list"]);
+
+            // Recursively call this function until we have all categories
+            if (response["data"]["search"]["trendingCategories"]["pageInfo"]["hasNextPage"] === true) {
+                const next = response["data"]["search"]["trendingCategories"]["pageInfo"]["endCursor"];
+                return this.getCategories(next, count, currentList);
+            }
+
+            return currentList;
         });
     }
 
