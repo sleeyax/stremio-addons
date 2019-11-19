87f5e47a75b2fbacf417c2902c0e974dbaff9d7d
Sleeyax <yourd3veloper@gmail.com>
2019-11-19 22:55:56 +0100

    addon/rarbg-torrents: generate random string, check if UHD

--- packages/addons/stremio-rarbg-torrents/src/utils.js
+++ packages/addons/stremio-rarbg-torrents/src/utils.js
@@ -14,18 +14,37 @@ function toHomanReadable(bytes) {
     return bytes.toFixed(1) +" " + units[i];
 }
 
+function is4k(str) {
+    str = str.toLowerCase();
+    return str.indexOf("2160p") !== -1 || str.indexOf("4k") !== -1 || str.indexOf("uhd") !== -1;
+}
+
 module.exports = {
     toStream: (torrentResults) => {
-        return torrentResults.torrent_results.map(result => {
+        return torrentResults.map(result => {
             const torrent = torrentParser(result.download);
             return {
-                name: 'RARBG',
+                name: is4k(result.category) ? 'RARBG-4K' : 'RARBG',
                 title: `${result.title}, C: ${result.category}, S: ${result.seeders} L: ${result.leechers}, size: ${toHomanReadable(result.size)}`,
                 infoHash: torrent.infoHash
             };
         });
     },
+    delay: (t, v) => {
+        return new Promise(function(resolve) {
+            setTimeout(resolve.bind(null, v), t)
+        });
+    },
     toZeroPaddedNumber: (num) => {
         return num <= 9 ? '0' + num : num;
     },
+    genRandomString: (min = 5, max = 10) => {
+        const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        const random = Math.floor(Math.random() * max) + min;
+        let result = "";
+        for (let i=0; i<=random; i++) {
+            result += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return result;
+    }
 };
