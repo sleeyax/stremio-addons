fc394c74cd562aa1d71cbc3a972744a52ffd2891
Sleeyax <yourd3veloper@gmail.com>
2019-11-19 22:56:25 +0100

    addon/rarbg-torrents: rename some values

---
+++ packages/addons/stremio-rarbg-torrents/src/categories.js
@@ -0,0 +1,35 @@
+module.exports = {
+    xxx: 4,
+    movies: {
+        x264: 17,
+        x264_1080: 44,
+        x264_3D: 47,
+        x264_4k: 50,
+        x265_4k: 51,
+        x265_4k_hdr: 52,
+        x264_720: 45,
+        xvid: 14,
+        xvid_720: 48,
+        full_bd: 42,
+        bd_remux: 46,
+    },
+    tv: {
+        normal: 18,
+        HD: 41,
+        UHD: 49
+    },
+    music: {
+        FLAC: 25,
+        MP3: 23
+    },
+    games: {
+        XBOX_360: 32,
+        PC_ISO: 27,
+        PC_RIP: 28,
+        PS3: 40,
+        PS4: 53,
+    },
+    software: {
+        PC_ISO: 33
+    },
+};
