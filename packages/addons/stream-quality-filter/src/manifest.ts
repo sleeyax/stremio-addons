4a84ad2fa57610ede2841bc7061118698490632b
Sleeyax <yourd3veloper@gmail.com>
2020-02-02 18:11:26 +0100

    Create stream-quality-filter addon

---
+++ packages/addons/stream-quality-filter/src/manifest.ts
@@ -0,0 +1,17 @@
+import {Manifest, ContentType} from 'stremio-addon-sdk';
+// require so package.json doens't get copied to build folder
+const {version, description} = require('../package.json');
+
+const manifest: Manifest = {
+   id: 'com.sleeyax.stream-quality-filter',
+   name: 'Stream Quality Filter',
+   description,
+   logo: 'https://i.imgur.com/iUBNpZM.png',
+   catalogs: [],
+   resources: ['stream'],
+   types: [ContentType.MOVIE, ContentType.SERIES],
+   version,
+   idPrefixes: ['tt']
+};
+
+export default manifest;
---
+++ packages/addons/stream-quality-filter/src/serverless.ts
@@ -0,0 +1,18 @@
+import addonInterface from './addon';
+import {getRouter} from 'stremio-addon-sdk';
+import landingTemplate from 'stremio-addon-sdk/src/landingTemplate';
+
+module.exports = (req, res) => {
+    const router = getRouter(addonInterface);
+    const landingHTML = landingTemplate(addonInterface.manifest);
+
+    router.get('/', (_, res) => {
+        res.setHeader('content-type', 'text/html');
+        res.end(landingHTML);
+    });
+
+    router(req, res, () => {
+        res.statusCode = 404;
+        res.end();
+    });
+};
