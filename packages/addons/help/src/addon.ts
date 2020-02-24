dcb407c0d9e114755392eb70cef5727727344bfd
Sleeyax <yourd3veloper@gmail.com>
2020-02-24 21:28:25 +0100

    Add tools

--- packages/addons/help/src/addon.ts
+++ packages/addons/help/src/addon.ts
@@ -1,14 +1,17 @@
 import { addonBuilder, MetaPreview, Stream } from "stremio-addon-sdk";
 import manifest from './manifest';
-import communities, { Community } from "./data/communities";
+import { Data, communities, tools } from "./data";
 
 const builder = new addonBuilder(manifest);
 
 builder.defineCatalogHandler(({id}) => {
     let metas: MetaPreview[] = [];
 
-    if (id.startsWith('communities'))
+    if (id.startsWith('communities')) {
         metas = communities;
+    } else if (id.startsWith('tools')) {
+        metas = tools;
+    }
         
     return Promise.resolve({metas});
 });
@@ -17,15 +20,21 @@ builder.defineStreamHandler(({id}) => {
     let streams: Stream[] = [];
 
     const type = id.split(':')[1];
+
+    let data: Data;
     
     if (type == 'community') {
-        const selectedCommunity: Community = communities.find(com => com.id == id);
-        streams = selectedCommunity.links.map(link => (<Stream>{
+        data = communities.find(com => com.id == id);
+    } else if (type == 'tools') {
+        data = tools.find(tool => tool.id == id);
+    }
+    
+    if (data)
+        streams = data.links.map(link => (<Stream>{
             name: 'help',
             title: link.name,
             externalUrl: link.url
         }));
-    }
 
     return Promise.resolve({streams});
 });
