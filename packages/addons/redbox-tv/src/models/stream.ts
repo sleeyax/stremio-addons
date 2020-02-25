9a8f1bbd0462c6b250ddb33517e89c2fed6bb127
Sleeyax <yourd3veloper@gmail.com>
2020-02-25 23:31:21 +0100

    implement type 48

--- packages/addons/redbox-tv/src/models/stream.ts
+++ packages/addons/redbox-tv/src/models/stream.ts
@@ -9,10 +9,11 @@ export default class RedBoxStream {
 
     async getM3u8Playlist() {
         let data: [string, string, (string, number) => {}];
+        let fixToken: (token: string, tokenLen: number) => string;
 
         switch (this.type) {
             case 38:
-                const fixToken = (token: string, tokenLen: number) => 
+                fixToken = (token: string, tokenLen: number) => 
                     [
                         token.substring(0, tokenLen - 59),
                         token.substring(tokenLen - 58, tokenLen - 52),
@@ -22,7 +23,25 @@ export default class RedBoxStream {
                     ].join('');
                 data = ['http://51.15.209.90:8800/fio/3b.rbt/', 'eWFyYXBuYWthYW1rYXJvOnR1bmduYWtpYWthcm8=', fixToken];
                 break;
+            case 48:
+                fixToken = (token: string, tokenLen: number) => {
+                    const now = new Date();
+                    const splitted = token.split('');
+                    // _in.pop(len(_in) + 2 - 3 - int(str(now.year)[:2]))
+                    splitted.splice(splitted.length + 2 - 3 - Number.parseInt(`${now.getFullYear()}`.substring(0, 2)), 1);
+                    // _in.pop(len(_in) + 3 - 4 - int(str(now.year)[2:]))
+                    splitted.splice(splitted.length + 3 - 4 - Number.parseInt(`${now.getFullYear()}`.substring(2)), 1);
+                    // _in.pop(len(_in) + 4 - 5 - (now.month - 1 + 1 + 10))
+                    splitted.splice(splitted.length + 4 - 5 - ((now.getMonth() + 1) - 1 + 1 + 10), 1);
+                    // _in.pop(len(_in) + 5 - 6 - now.day)
+                    splitted.splice(splitted.length + 5 - 6 - now.getDate(), 1);
+                    return splitted.join('');
+                };
+                data = ['http://51.15.209.90:8800/cip/4c.rbt/', 'QDA3NzEyMSM6QDA3NzEyMSM=', fixToken];
+                break;
             default:
+                // console.log(this.mediaUrl);
+                // TODO: other types (0, 18, 21, ...)
                 throw new Error('unsupported type ' + this.type);
         }
 
