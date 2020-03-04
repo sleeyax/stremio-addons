63d6a472332954ce5504a95f1f8ab8a8aece3b42
Sleeyax <yourd3veloper@gmail.com>
2020-03-04 22:00:19 +0100

    addon/stream-quality-filter: improve code a bit, catch errors

--- packages/addons/stream-quality-filter/src/streams.ts
+++ packages/addons/stream-quality-filter/src/streams.ts
@@ -11,7 +11,9 @@ export async function getAllStreams(imdbid: string, type: string = 'movie') {
     const streamPromises = urls.map(url => needle('get', `${url}/stream/${type}/${imdbid}.json`));
     const streams = await Promise.all(streamPromises)
         .then((results: NeedleResponse[]) => results
-            .map(result => result.body.streams)
-            .reduce((acc, val) => acc.concat(val), []));
+            .map(result => result.body.streams || [])
+            .reduce((acc, val) => acc.concat(val), []))
+        .catch(err => console.error(err));
+
     return streams;
 }
