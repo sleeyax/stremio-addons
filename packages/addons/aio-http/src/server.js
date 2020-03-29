6b3f7f6a1c61ea8f8f72cf1f12ec17f91a141806
Sleeyax <yourd3veloper@gmail.com>
2020-03-29 14:37:24 +0200

    addon/aio-http: load addons beforehand

--- packages/addons/aio-http/src/server.js
+++ packages/addons/aio-http/src/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const {join} = require('path');
 const {promisify} = require('util');
 const fs = require('fs');
-const pathExists = promisify(fs.exists);
 const readDir = promisify(fs.readdir);
 
 const app = express();
@@ -16,44 +15,56 @@ const aliases = {
 };
 
 /**
- * Tries to load the serverless function of specified addon.
- * Returns null if it doesn't exist.
- * @param {string} addon name of the addon
+ * Load all addon modules dynamically
  */
-async function loadServerlessFunc(addon) {
-    const addonDir = join(__dirname, '..', '..', addon);
-    
-    // load .env file if it exists
-    const envFile = join(addonDir, '.env');
-    if (await pathExists(envFile))
-        config({path: envFile});
+function initModules() {
+    const rootDir = join(__dirname, '..', '..');
 
-    // load the addon
-    let serverlessFile;
-    const dirs = ['src', 'build'];
-    for (const i in dirs) {
-        const dir = dirs[i];
-        const path = join(addonDir, dir, 'serverless.js');
-        if (await pathExists(path)) {
-            serverlessFile = path;
-            break;
+    const addonDirectories = fs.readdirSync(rootDir, { withFileTypes: true })
+        .filter(dir => dir.isDirectory() && !dir.name.startsWith('aio'))
+        .map(dir => dir.name);
+
+    const serverlessModules = {};
+
+    addonDirectories.forEach(addonDirName => {
+        const addonDir = join(rootDir, addonDirName);
+
+        // load .env file if it exists
+        const envFile = join(addonDir, '.env');
+        if (fs.existsSync(envFile))
+            config({path: envFile});
+
+        // load the addon
+        const sourceDirs = ['src', 'build'];
+        for (const i in sourceDirs) {
+            const sourceDir = sourceDirs[i];
+            const path = join(addonDir, sourceDir, 'serverless.js');
+            if (fs.existsSync(path)) {
+                serverlessModules[addonDirName] = require(path);
+                break;
+            }
         }
-    }
+    });
 
-    return serverlessFile ? require(serverlessFile) : null;
+    console.log(`Initialized ${Object.keys(serverlessModules).length} addons`);
+
+    return serverlessModules;
 }
+
+const addonModules = initModules();
+
+const loadAddon = (name) => addonModules[name] || null;
 
 app.get('/', (_, res) => res.sendFile(join(__dirname, 'static', 'index.html')));
 
-app.get('/:addon/:route(*)', async (req, res) => {
+app.get('/:addon/:route(*)', (req, res) => {
     const addonName = aliases[req.params.addon] || req.params.addon;
 
-    const serverlessFunc = await loadServerlessFunc(addonName);
+    const serverlessFunc = loadAddon(addonName);
     
     if (serverlessFunc != null) {
         req.url = '/' + req.params.route;
         serverlessFunc(req, res);
-
     } else {
         res.send('Not found').status(404);
     }
