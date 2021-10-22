b451024663407657dc221bb23d1e877abbf781e5
Sleeyax <yourd3veloper@gmail.com>
2021-10-23 01:34:39 +0200

    addon/xnss: update port

--- packages/addons/xnxx/src/server.ts
+++ packages/addons/xnxx/src/server.ts
@@ -2,5 +2,5 @@ import { serveHTTP } from "stremio-addon-sdk";
 import initAddon from "./addon";
 
 initAddon()
-    .then((addonInterface) => serveHTTP(addonInterface, {port: +process.env.PORT || +process.argv[2] || 69}))
+    .then((addonInterface) => serveHTTP(addonInterface, {port: +process.env.PORT || +process.argv[2] || 6969}))
     .catch(ex => console.error(ex));
